bd4eba520f48c9772de105107a3b874d75d890af
lukdown <lukdown10@naver.com>
2025-03-03 17:55:29 +0900

    grafana 대시보드 추가(로컬서버로 적용 완료)

--- src/components/dashboard/DashboardPage.tsx
+++ src/components/dashboard/DashboardPage.tsx
@@ -1,11 +1,19 @@
 import Footer from "@/components/common/Footer";
 
-function DashboardPage() {
+const DashboardPage = () => {
   return (
-    <div>dashboard
-      <Footer/>
-    </div>    
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-2xl font-bold mb-4">Grafana Dashboard</h1>
+      <iframe
+        src="http://localhost:3000/goto/lJbVFApHR?orgId=1"
+        width="100%"
+        height="600px"
+        frameBorder="0"
+        allowFullScreen
+      ></iframe>
+      <Footer />
+    </div>
   );
-}
+};
 
 export default DashboardPage;
